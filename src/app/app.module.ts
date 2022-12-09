5f994bbb742e89a909a433f0140b71e7aa6c05b8
fawzy20 <fawzy-elsam@outlook.fr>
2022-12-09 20:20:05 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    MatSlideToggleModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import productData from '../product.json';  
+
+interface Product {  
+  id: Number;  
+  name: String;  
+  specifications: any;
+  picture: String;  
+}  
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+
+export class MainComponent {
+
+  products: Product[] = productData;  
+
+}
